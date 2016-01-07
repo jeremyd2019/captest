0efa82b09bc3714a98f4708c854c521ec88470b3
Jeremy Drake <github@jdrake.com>
2016-01-06 22:57:54 -0800

    switch to using URL.openStream to get the alert xml instead of the apache http lib, which has been deprecated in newer versions of android

--- android/app/src/main/java/com/jdrake/apps/captest/AlertDetailFragment.java
+++ android/app/src/main/java/com/jdrake/apps/captest/AlertDetailFragment.java
@@ -9,21 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.jdrake.apps.captest.xml.Alert;
 import com.jdrake.apps.captest.xml.DateFormatTransformer;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 import org.simpleframework.xml.transform.RegistryMatcher;
 
 import java.io.IOException;
-import java.net.HttpCookie;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -63,40 +59,45 @@ public class AlertDetailFragment extends Fragment {
             // to load content from a content provider.
             // TODO load alert
             //mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
-            new AsyncTask<String, Void, Alert>() {
+            try {
+                new AsyncTask<URL, Void, Alert>() {
 
-                @Override
-                protected Alert doInBackground(String... strings) {
-                    try {
+                    @Override
+                    protected Alert doInBackground(URL... urls) {
+                        try {
+                            RegistryMatcher m = new RegistryMatcher();
+                            // note we should support more formats than this for iso8601
+                            m.bind(Date.class, new DateFormatTransformer(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ", Locale.US)));
+                            Serializer serializer = new Persister(m);
 
-
-                        HttpClient client = new DefaultHttpClient();
-                        HttpGet get = new HttpGet(strings[0]);
-                        HttpResponse response = client.execute(get);
-                        RegistryMatcher m = new RegistryMatcher();
-                        // note we should support more formats than this for iso8601
-                        m.bind(Date.class, new DateFormatTransformer(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ", Locale.US)));
-                        Serializer serializer = new Persister(m);
-                        return serializer.read(Alert.class, EntityUtils.toString(response.getEntity()), false);
-                    } catch (IOException e) {
-                        Log.e("AlertDetailFragment", String.format("IOException getting alert detail for %s", strings[0]), e);
-                        return null;
-                    } catch (Exception e)
-                    {
-                        Log.e("AlertDetailFragment", String.format("Exception getting alert detail for %s", strings[0]), e);
-                        return null;
+                            InputStream is = urls[0].openStream();
+                            try {
+                                return serializer.read(Alert.class, is, false);
+                            } finally {
+                                is.close();
+                            }
+                        } catch (IOException e) {
+                            Log.e("AlertDetailFragment", String.format("IOException getting alert detail for %s", urls[0].toString()), e);
+                            return null;
+                        } catch (Exception e) {
+                            Log.e("AlertDetailFragment", String.format("Exception getting alert detail for %s", urls[0].toString()), e);
+                            return null;
+                        }
                     }
-                }
 
-                @Override
-                protected void onPostExecute(Alert alert) {
-                    mItem = alert;
-                    if (getView() != null)
-                    {
-                        ((TextView)getView().findViewById(R.id.alert_detail)).setText(alert.getInfo().get(0).getDescription());
+                    @Override
+                    protected void onPostExecute(Alert alert) {
+                        mItem = alert;
+                        if (getView() != null) {
+                            ((TextView) getView().findViewById(R.id.alert_detail)).setText(alert.getInfo().get(0).getDescription());
+                        }
                     }
-                }
-            }.execute(getArguments().getString(ARG_ITEM_ID));
+                }.execute(new URL(getArguments().getString(ARG_ITEM_ID)));
+            }
+            catch (MalformedURLException e)
+            {
+                Log.e("AlertDetailFragment", String.format("Malformed url: %s", getArguments().getString(ARG_ITEM_ID)), e);
+            }
         }
     }
 
