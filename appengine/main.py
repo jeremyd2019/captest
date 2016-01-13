08a168d95b609b6762fd3f93dd699dd1076f7b68
Jeremy Drake <github@jdrake.com>
2016-01-12 22:35:05 -0800

    my setting indexed=False seems to have prevented hitting the datastore writes quota, now it is hitting the datastore small operations quota.  Use memcache to try to avoid querying the set of keys from the datastore every time

--- appengine/main.py
+++ appengine/main.py
@@ -2,22 +2,22 @@
 
 import webapp2
 import models
+from google.appengine.api import memcache
 from google.appengine.ext import ndb
 import capparser
 
 @ndb.tasklet
-def makeAlertKeySetAndFetchAlertsAtom():
-	"""
-	Walk and chew gum at the same time, through cooperative multitasking.  Unfortunately results in one of those doXAndY function names.
-	"""
-	keyset, response = yield models.Alert.makeKeySet(), ndb.get_context().urlfetch('http://alerts.weather.gov/cap/us.php?x=0')
-	raise ndb.Return((keyset, response.content))
+def fetchAlertsAtom():
+	response = yield ndb.get_context().urlfetch('http://alerts.weather.gov/cap/us.php?x=0')
+	raise ndb.Return(response.content)
 
 class MainHandler(webapp2.RequestHandler):
 	@ndb.toplevel
 	def get(self):
-		future = makeAlertKeySetAndFetchAlertsAtom()
-		oldkeys, xml = future.get_result()
+		xmlfuture = fetchAlertsAtom()
+		oldkeys = memcache.get('keys')
+		if oldkeys is None:
+			oldkeys = models.Alert.makeKeySet().get_result()
 		parser = capparser.Parser()
 		parser.alertFactory = lambda: models.Alert()
 		def setAlertId(alert, id):
@@ -25,7 +25,9 @@ class MainHandler(webapp2.RequestHandler):
 		parser.setAlertId = setAlertId
 		parser.geoPtFactory = models.GeoPt
 		counter = [0, 0]
+		curkeys = set()
 		def onAlertCreated(alert):
+			curkeys.add(alert.key)
 			if alert.key in oldkeys:
 				oldkeys.remove(alert.key)
 				counter[0] += 1
@@ -34,10 +36,11 @@ class MainHandler(webapp2.RequestHandler):
 				counter[1] += 1
 
 		parser.onAlertCreated = onAlertCreated
-		parser.parse(xml)
+		parser.parse(xmlfuture.get_result())
 
 		ndb.delete_multi_async(oldkeys)
 		self.response.write("Dupes ignored %d, put %d, deleted %d<br>\r\n" % (counter[0], counter[1], len(oldkeys)))
+		memcache.set('keys', curkeys)
 
 
 app = webapp2.WSGIApplication([
